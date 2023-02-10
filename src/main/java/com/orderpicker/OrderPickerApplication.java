95c2a7158b8b27f96e0513d67886f0265aa0c511
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-10 12:00:12 -0500

    Feat: Create database connection

---
+++ src/main/java/com/orderpicker/OrderPickerApplication.java
@@ -0,0 +1,13 @@
+package com.orderpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderpicker/OrderPickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderpicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
