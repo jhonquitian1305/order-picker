38f88549d3ca26220d884a9e142a5b53be67b3a4
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-10 15:27:41 -0500

    Init: create interface UserRepository(DAO) to manage queries of User to database

---
+++ src/main/java/com/orderpicker/user/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.orderpicker.user.domain.repository;
+
+import com.orderpicker.user.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
