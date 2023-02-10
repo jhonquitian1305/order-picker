e725d6041fbe6bd875099b084c2c8b435521eb54
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-10 15:17:00 -0500

    Feat: create mapUser method to map a UserDTO for a User

--- src/main/java/com/orderpicker/user/application/mapper/MapperUser.java
+++ src/main/java/com/orderpicker/user/application/mapper/MapperUser.java
@@ -1,7 +1,18 @@
 package com.orderpicker.user.application.mapper;
 
+import com.orderpicker.user.domain.model.User;
+import com.orderpicker.user.infrastructure.dto.UserDTO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MapperUser {
+    public User mapUser(UserDTO userDTO){
+        return User.builder()
+                .id(userDTO.getId())
+                .dni(userDTO.getDni())
+                .fullName(userDTO.getFullName())
+                .email(userDTO.getEmail())
+                .password(userDTO.getPassword())
+                .build();
+    }
 }
