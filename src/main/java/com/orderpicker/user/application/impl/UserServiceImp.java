636201d8df8c8294176bf9d01a6829fd0a0f88de
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-10 16:32:30 -0500

    Init: create UserServiceImp which implement UserService

---
+++ src/main/java/com/orderpicker/user/application/impl/UserServiceImp.java
@@ -0,0 +1,8 @@
+package com.orderpicker.user.application.impl;
+
+import com.orderpicker.user.infrastructure.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImp implements UserService {
+}
