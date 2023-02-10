251d864b96e79042cf492753dfddbde294aa2b3c
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-10 16:54:04 -0500

    Init: create UserController with its url to create endpoints

---
+++ src/main/java/com/orderpicker/user/infrastructure/constants/UserEndpointsConstants.java
@@ -0,0 +1,5 @@
+package com.orderpicker.user.infrastructure.constants;
+
+public class UserEndpointsConstants {
+    public static final String ENDPOINT_USERS="/api/order-picker/users";
+}
---
+++ src/main/java/com/orderpicker/user/infrastructure/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.orderpicker.user.infrastructure.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.orderpicker.user.infrastructure.constants.UserEndpointsConstants.ENDPOINT_USERS;
+
+@RestController
+@RequestMapping(ENDPOINT_USERS)
+public class UserController {
+}
