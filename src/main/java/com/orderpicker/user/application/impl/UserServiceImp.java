58025219ee5ef46ad1779f25a5b7e29e962d9d09
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-10 23:34:14 -0500

    Refactor: change exception NotFoundException to BadRequestException

--- src/main/java/com/orderpicker/user/application/impl/UserServiceImp.java
+++ src/main/java/com/orderpicker/user/application/impl/UserServiceImp.java
@@ -1,6 +1,6 @@
 package com.orderpicker.user.application.impl;
 
-import com.orderpicker.user.application.exception.UserNotFoundException;
+import com.orderpicker.user.application.exception.UserBadRequestException;
 import com.orderpicker.user.application.mapper.MapperUser;
 import com.orderpicker.user.domain.model.User;
 import com.orderpicker.user.domain.repository.UserRepository;
@@ -32,14 +32,14 @@ public class UserServiceImp implements UserService {
     protected void findByDni(String dni){
         Optional<User> userFound = this.userRepository.findByDni(dni);
         if(userFound.isPresent()){
-            throw new UserNotFoundException("The user with dni %s already exist".formatted(dni));
+            throw new UserBadRequestException("The user with dni %s already exist".formatted(dni));
         }
     }
 
     protected void findByEmail(String email){
         Optional<User> userFound = this.userRepository.findByEmail(email);
         if(userFound.isPresent()){
-            throw new UserNotFoundException("The user with email %s already exist".formatted(email));
+            throw new UserBadRequestException("The user with email %s already exist".formatted(email));
         }
     }
 }
