49e8b333a35f6685daccd410104e0aa7e7dd4558
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-10 22:38:58 -0500

    Feat: create method to verify if a user exists by dni

--- src/main/java/com/orderpicker/user/application/impl/UserServiceImp.java
+++ src/main/java/com/orderpicker/user/application/impl/UserServiceImp.java
@@ -1,8 +1,26 @@
 package com.orderpicker.user.application.impl;
 
+import com.orderpicker.user.application.exception.UserNotFoundException;
+import com.orderpicker.user.domain.model.User;
+import com.orderpicker.user.domain.repository.UserRepository;
 import com.orderpicker.user.infrastructure.service.UserService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor(onConstructor =  @__(@Autowired))
 public class UserServiceImp implements UserService {
+
+    private final @NonNull UserRepository userRepository;
+
+    protected void findByDni(String dni){
+        Optional<User> userFound = this.userRepository.findByDni(dni);
+        if(userFound.isPresent()){
+            throw new UserNotFoundException("The user with dni %s already exist".formatted(dni));
+        }
+    }
 }
