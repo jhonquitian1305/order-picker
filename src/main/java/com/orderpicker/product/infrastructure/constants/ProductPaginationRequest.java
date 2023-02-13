ddd1f9ce824f712d383056096527aee3ebb61f42
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-13 13:52:30 -0500

    Feat: create PaginationRequest to have default values

---
+++ src/main/java/com/orderpicker/product/infrastructure/constants/ProductPaginationRequest.java
@@ -0,0 +1,8 @@
+package com.orderpicker.product.infrastructure.constants;
+
+public class ProductPaginationRequest {
+    public static final String PRODUCT_DEFAULT_NUMBER_PAGE="0";
+    public static final String PRODUCT_DEFAULT_PAGE_SIZE="10";
+    public static final String PRODUCT_DEFAULT_SORT_BY="id";
+    public static final String PRODUCT_DEFAULT_SORT_DIR="asc";
+}
