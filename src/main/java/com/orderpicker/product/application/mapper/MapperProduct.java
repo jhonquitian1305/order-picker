a78aa385859a4ff776e829d2242881bf1a3be1e1
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-12 20:33:35 -0500

    Feat: create mapProduct method to map a ProductDTO for a Product

--- src/main/java/com/orderpicker/product/application/mapper/MapperProduct.java
+++ src/main/java/com/orderpicker/product/application/mapper/MapperProduct.java
@@ -1,7 +1,16 @@
 package com.orderpicker.product.application.mapper;
 
+import com.orderpicker.product.domain.model.Product;
+import com.orderpicker.product.infrastructure.dto.ProductDTO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MapperProduct {
+    public Product mapProduct(ProductDTO productDTO){
+        return Product.builder()
+                .id(productDTO.getId())
+                .name(productDTO.getName())
+                .amount(productDTO.getAmount())
+                .build();
+    }
 }
