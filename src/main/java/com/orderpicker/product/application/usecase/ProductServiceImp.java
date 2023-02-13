0e8dd0f8c9caf4f55b7ff8521becd417dc4766f3
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-12 21:20:32 -0500

    Init: create ProductServiceImp which implement ProductService

---
+++ src/main/java/com/orderpicker/product/application/usecase/ProductServiceImp.java
@@ -0,0 +1,8 @@
+package com.orderpicker.product.application.usecase;
+
+import com.orderpicker.product.infrastructure.service.ProductService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImp implements ProductService {
+}
