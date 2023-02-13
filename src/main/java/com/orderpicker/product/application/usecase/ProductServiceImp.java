c82e0a64de6c1aa46180213c5d107afba5aa194b
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-13 11:00:24 -0500

    Feat: create method to verify if a product exists by name

--- src/main/java/com/orderpicker/product/application/usecase/ProductServiceImp.java
+++ src/main/java/com/orderpicker/product/application/usecase/ProductServiceImp.java
@@ -1,8 +1,26 @@
 package com.orderpicker.product.application.usecase;
 
+import com.orderpicker.product.application.exception.ProductBadRequestException;
+import com.orderpicker.product.domain.model.Product;
+import com.orderpicker.product.domain.repository.ProductRepository;
 import com.orderpicker.product.infrastructure.service.ProductService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor(onConstructor =  @__(@Autowired))
 public class ProductServiceImp implements ProductService {
+
+    private final @NonNull ProductRepository productRepository;
+
+    protected void findByName(String name){
+        Optional<Product> productFound = this.productRepository.findByName(name);
+        if(productFound.isEmpty()){
+            throw new ProductBadRequestException("Product with name %s already exists".formatted(name));
+        }
+    }
 }
