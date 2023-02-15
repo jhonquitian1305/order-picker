81a4ee14e374c62af1e80fdbbdeffbc0e83f6d33
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-15 15:04:40 -0500

    Init: create OrderServiceImp which implement OrderService

---
+++ src/main/java/com/orderpicker/order/application/usecase/OrderServiceImp.java
@@ -0,0 +1,8 @@
+package com.orderpicker.order.application.usecase;
+
+import com.orderpicker.order.infrastructure.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImp implements OrderService {
+}
