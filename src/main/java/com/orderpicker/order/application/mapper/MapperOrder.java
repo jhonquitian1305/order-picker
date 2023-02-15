fd5bf252a2a3420f7ebad582d49eae1189e7693c
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-15 15:56:08 -0500

    Feat: Map Order model to OrderDTOResponse

--- src/main/java/com/orderpicker/order/application/mapper/MapperOrder.java
+++ src/main/java/com/orderpicker/order/application/mapper/MapperOrder.java
@@ -2,6 +2,7 @@ package com.orderpicker.order.application.mapper;
 
 import com.orderpicker.order.domain.model.Order;
 import com.orderpicker.order.infrastructure.dto.OrderDTO;
+import com.orderpicker.order.infrastructure.response.OrderDTOResponse;
 import com.orderpicker.product.domain.model.Product;
 import com.orderpicker.user.domain.model.User;
 import org.springframework.stereotype.Component;
@@ -18,6 +19,18 @@ public class MapperOrder {
                 .isDelivered(orderDTO.isDelivered())
                 .client(client)
                 .products(products)
+                .build();
+    }
+
+    public OrderDTOResponse mapOrderDTOResponse(Order order){
+        return OrderDTOResponse.builder()
+                .id(order.getId())
+                .description(order.getDescription())
+                .totalPrice(order.getTotalPrice())
+                .isDelivered(order.isDelivered())
+                .client(order.getClient())
+                .products(order.getProducts())
+                .createdAt(order.getCreatedAt())
                 .build();
     }
 }
