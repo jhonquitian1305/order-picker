270567a5e3627643a9e572ba8b88d1645175dced
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-15 14:56:28 -0500

    Init: create OrderRepository(DAO) interface to manage queries of Order to database

---
+++ src/main/java/com/orderpicker/order/domain/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.orderpicker.order.domain.repository;
+
+import com.orderpicker.order.domain.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
