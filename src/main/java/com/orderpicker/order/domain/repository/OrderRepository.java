7dbc08aa0868e61782733d3b37d9daf8762ac28b
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-18 18:34:13 -0500

    Feat: create findByUser method with query to get all user's orders

--- src/main/java/com/orderpicker/order/domain/repository/OrderRepository.java
+++ src/main/java/com/orderpicker/order/domain/repository/OrderRepository.java
@@ -1,9 +1,19 @@
 package com.orderpicker.order.domain.repository;
 
 import com.orderpicker.order.domain.model.Order;
+import com.orderpicker.order.infrastructure.dto.OrderUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("""
+            SELECT o.id as id, o.orderDescription as orderDescription, o.isDelivered as isDelivered,
+            o.totalPrice as totalPrice, o.createdAt as createdAt from orders o
+            inner join users u on o.client = u.id            
+            where u.id = ?1""")
+    Page<OrderUser> findByUser(Long id, Pageable pageable);
 }
