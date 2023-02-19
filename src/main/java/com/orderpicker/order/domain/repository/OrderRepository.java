96e3d47a1cdf98bdff5ad6ebdafac9772f940b8e
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-19 09:09:23 -0500

    Feat: create getAll method with query to get all orders ordered with user's information

--- src/main/java/com/orderpicker/order/domain/repository/OrderRepository.java
+++ src/main/java/com/orderpicker/order/domain/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.orderpicker.order.domain.repository;
 
 import com.orderpicker.order.domain.model.Order;
 import com.orderpicker.order.infrastructure.dto.OrderUser;
+import com.orderpicker.order.infrastructure.dto.Orders;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -16,4 +17,11 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
             inner join users u on o.client = u.id            
             where u.id = ?1""")
     Page<OrderUser> findByUser(Long id, Pageable pageable);
+
+    @Query("""
+            SELECT o.id as id, u.fullName as user, u.email as userEmail, o.orderDescription as orderDescription, o.isDelivered as isDelivered,
+            o.totalPrice as totalPrice, o.createdAt as createdAt from orders o
+            INNER JOIN users u on o.client = u.id
+            """)
+    Page<Orders> getAll(Pageable pageable);
 }
