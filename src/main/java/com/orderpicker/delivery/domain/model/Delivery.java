78918824853c5e6ab505be4b2b74fcf36725a9b9
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-20 16:14:36 -0500

    Feat: create Delivery class

---
+++ src/main/java/com/orderpicker/delivery/domain/model/Delivery.java
@@ -0,0 +1,33 @@
+package com.orderpicker.delivery.domain.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "deliveries")
+public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long delivery;
+
+    @Column(name = "total_cost", nullable = false)
+    @Positive
+    private double totalCost;
+
+    @Column(name = "is_completed")
+    @Value(value = "false")
+    private boolean isCompleted;
+
+    @Column(name = "is_payed")
+    @Value(value = "false")
+    private boolean isPayed;
+
+    @Column(nullable = false)
+    private double off;
+}
