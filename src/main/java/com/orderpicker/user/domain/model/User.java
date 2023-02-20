0f0a14dc8e12ccd84e62d3c9ce2b62b3a5f1239c
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-20 10:08:13 -0500

    Feat: Add address and phone attributes to the User model

--- src/main/java/com/orderpicker/user/domain/model/User.java
+++ src/main/java/com/orderpicker/user/domain/model/User.java
@@ -1,6 +1,7 @@
 package com.orderpicker.user.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -29,6 +30,14 @@ public class User {
 
     @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
+    @Size(min = 5, max = 100)
+    private String address;
+
+    @Column(nullable = false)
+    @Size(min = 7, max = 20)
+    private String phone;
 
     @CreationTimestamp
     @Column(name = "created_at")
