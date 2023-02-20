43c7329717588242780a0dc7f103ca31ef2586bf
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-20 16:48:11 -0500

    Init: create DeliveryServiceImp which implement DeliveryService

---
+++ src/main/java/com/orderpicker/delivery/application/usecase/DeliveryServiceImp.java
@@ -0,0 +1,8 @@
+package com.orderpicker.delivery.application.usecase;
+
+import com.orderpicker.delivery.infrastructure.service.DeliveryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeliveryServiceImp implements DeliveryService {
+}
