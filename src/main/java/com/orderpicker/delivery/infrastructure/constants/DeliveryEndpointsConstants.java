486584d81ac03ec9fe043fcf2922b08a607208a6
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-20 16:51:34 -0500

    Init: create DeliveryController with its url to create endpoints

---
+++ src/main/java/com/orderpicker/delivery/infrastructure/constants/DeliveryEndpointsConstants.java
@@ -0,0 +1,5 @@
+package com.orderpicker.delivery.infrastructure.constants;
+
+public class DeliveryEndpointsConstants {
+    public static final String ENDPOINT_DELIVERIES="/api/order-picker/deliveries";
+}
---
+++ src/main/java/com/orderpicker/delivery/infrastructure/controller/DeliveryController.java
@@ -0,0 +1,11 @@
+package com.orderpicker.delivery.infrastructure.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.orderpicker.delivery.infrastructure.constants.DeliveryEndpointsConstants.ENDPOINT_DELIVERIES;
+
+@RestController
+@RequestMapping(ENDPOINT_DELIVERIES)
+public class DeliveryController {
+}
