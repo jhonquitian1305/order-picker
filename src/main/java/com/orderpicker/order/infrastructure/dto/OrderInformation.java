fde1fc7a297c46c3d00f55a26540bd185cd0497f
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-21 11:02:10 -0500

    Refactor: change String type to Double

--- src/main/java/com/orderpicker/order/infrastructure/dto/OrderInformation.java
+++ src/main/java/com/orderpicker/order/infrastructure/dto/OrderInformation.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface OrderInformation {
     Long getId();
     List<String> getOrderDescription();
-    String getTotalPrice();
+    Double getTotalPrice();
     Timestamp getCreatedAt();
     boolean getIsDelivered();
 }
