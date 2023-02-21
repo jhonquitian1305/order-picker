81e70054eddb28f833784fa278ed6c7d249cd262
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-21 10:29:58 -0500

    Refactor: Remove validation Is empty innecesary

--- src/main/java/com/orderpicker/delivery/infrastructure/dto/DeliveryDTO.java
+++ src/main/java/com/orderpicker/delivery/infrastructure/dto/DeliveryDTO.java
@@ -1,7 +1,5 @@
 package com.orderpicker.delivery.infrastructure.dto;
 
-import com.orderpicker.order.domain.model.Order;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
@@ -17,7 +15,6 @@ public class DeliveryDTO {
     private Long delivery;
 
     @NotNull(message = "order field must have one value")
-    @NotEmpty(message = "order field must have one value")
     private Long order;
 
     private double totalCost;
@@ -27,7 +24,6 @@ public class DeliveryDTO {
     private boolean isPayed;
 
     @NotNull(message = "off field couldn't be null")
-    @NotEmpty(message = "off field couldn't be empty")
     @Positive
     private double off;
 
