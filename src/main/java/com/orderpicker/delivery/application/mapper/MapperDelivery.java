b215e448c628b62f5d92ea4e08cab6af447ea982
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-21 11:13:39 -0500

    Feat: Add createDelivery method to assign it the order

--- src/main/java/com/orderpicker/delivery/application/mapper/MapperDelivery.java
+++ src/main/java/com/orderpicker/delivery/application/mapper/MapperDelivery.java
@@ -1,7 +1,19 @@
 package com.orderpicker.delivery.application.mapper;
 
+import com.orderpicker.delivery.domain.model.Delivery;
+import com.orderpicker.delivery.infrastructure.dto.DeliveryDTO;
+import com.orderpicker.delivery.infrastructure.response.DeliveryDTOResponse;
+import com.orderpicker.order.domain.model.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MapperDelivery {
+    public Delivery createDelivery(DeliveryDTO deliveryDTO, Order order){
+        return Delivery.builder()
+                .delivery(deliveryDTO.getDelivery())
+                .totalCost(deliveryDTO.getTotalCost())
+                .off(deliveryDTO.getOff())
+                .order(order)
+                .build();
+    }
 }
