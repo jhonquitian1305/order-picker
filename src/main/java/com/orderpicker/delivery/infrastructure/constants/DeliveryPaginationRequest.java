905089e79ecac4fd92843b0852a94928bc6a4b8b
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-02-24 16:31:31 -0500

    Feat: create PaginationRequest to have default values when deliveries are get

---
+++ src/main/java/com/orderpicker/delivery/infrastructure/constants/DeliveryPaginationRequest.java
@@ -0,0 +1,8 @@
+package com.orderpicker.delivery.infrastructure.constants;
+
+public class DeliveryPaginationRequest {
+    public static final String DELIVERY_DEFAULT_NUMBER_PAGE="0";
+    public static final String DELIVERY_DEFAULT_PAGE_SIZE="10";
+    public static final String DELIVERY_DEFAULT_SORT_BY="id";
+    public static final String DELIVERY_DEFAULT_SORT_DIR="asc";
+}
