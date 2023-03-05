ee1255fbad50d86f779ebd068a66c46dc34e7d99
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-03-05 13:40:30 -0500

    Feat: Add SecurityConfiguration class where manage the permissions according to the role

---
+++ src/main/java/com/orderpicker/security/config/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.orderpicker.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/order-picker/products/**")
+                .hasAnyAuthority("ADMIN", "EMPLOYEE")
+                .requestMatchers("/api/order-picker/deliveries/**")
+                .hasAnyAuthority("ADMIN", "EMPLOYEE")
+                .anyRequest()
+                .permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(this.authenticationProvider)
+                .addFilterBefore(this.jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
