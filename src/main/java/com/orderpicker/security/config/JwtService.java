d0be0d3c33f02444c7a6e3dcddb64335bf1cc4ba
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-03-05 13:06:44 -0500

    Feat: Add Service annotation

--- src/main/java/com/orderpicker/security/config/JwtService.java
+++ src/main/java/com/orderpicker/security/config/JwtService.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@Service
 public class JwtService {
     private final static String SECRET_KEY = "597033733676397924423F4528482B4D6251655468576D5A7134743777217A25";
 
