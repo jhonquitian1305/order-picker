5c86c7b332e8c7eb152e28adcd30b4016a6b16ed
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-03-05 12:56:03 -0500

    Feat: create ApplicationConfig class where get a user by email and provided auth with encrypted password

---
+++ src/main/java/com/orderpicker/security/config/ApplicationConfig.java
@@ -0,0 +1,43 @@
+package com.orderpicker.security.config;
+
+import com.orderpicker.user.infrastructure.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@AllArgsConstructor
+public class ApplicationConfig {
+
+    private final UserService userService;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return this.userService::getByEmail;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authorizationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
