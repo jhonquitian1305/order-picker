d425e12304609ce52ddf89a3c28b349f364acf83
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-04-07 20:51:01 -0500

    Doc: Create documentation to user's module

--- src/main/java/com/orderpicker/user/infrastructure/controller/UserController.java
+++ src/main/java/com/orderpicker/user/infrastructure/controller/UserController.java
@@ -6,6 +6,12 @@ import com.orderpicker.user.application.mapper.MapperUser;
 import com.orderpicker.user.infrastructure.dto.UserDTO;
 import com.orderpicker.user.infrastructure.response.UserResponse;
 import com.orderpicker.user.infrastructure.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,28 +35,95 @@ public class UserController {
 
     private final MapperUser mapperUser;
 
+    @Operation(summary = "Create a User")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "User Created",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDTO.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid values entry",
+                    content = @Content
+            )
+    })
     @PostMapping()
-    ResponseEntity<UserDTO> save(@Valid @RequestBody UserDTO userDTO, BindingResult bindingResult){
+    ResponseEntity<UserDTO> save(
+            @Valid @RequestBody UserDTO userDTO, BindingResult bindingResult
+    ){
         if(bindingResult.hasErrors()){
             throw new UserBadRequestException(Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage());
         }
         return new ResponseEntity<>(this.mapperUser.mapUserDTO(this.userService.save(userDTO)), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get a User by id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User Found",
+                    content = {
+                            @Content(
+                                mediaType = "application/json",
+                                schema = @Schema(implementation = UserDTO.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "No permission to apply",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "User Not Found",
+                    content = @Content
+            )
+    })
     @GetMapping(ENDPOINT_USER_ID)
     ResponseEntity<UserDTO> getById(
-        @PathVariable("id") Long id
+            @Parameter(description = "User ID to search")
+            @PathVariable("id") Long id
     ){
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
         this.userService.validateUserRequestById(id, userEmail);
         return new ResponseEntity<>(this.mapperUser.mapUserDTO(this.userService.getById(id)), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get All Users")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Get All Users",
+                    content = {
+                            @Content(
+                                mediaType = "application/json",
+                                schema = @Schema(implementation = UserResponse.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "No permission to apply",
+                    content = @Content
+            )
+        }
+    )
     @GetMapping
     ResponseEntity<UserResponse> findAll(
+            @Parameter(description = "Choose a page number in the search")
             @RequestParam(value = "pageNumber", defaultValue = USER_DEFAULT_NUMBER_PAGE, required = false) int pageNumber,
+            @Parameter(description = "Choose a page size in the search")
             @RequestParam(value = "pageSize", defaultValue = USER_DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @Parameter(description = "Sort the answer by a field")
             @RequestParam(value = "sortBy", defaultValue = USER_DEFAULT_SORT_BY, required = false) String sortBy,
+            @Parameter(description = "Sort the answer by a direction")
             @RequestParam(value = "sortDir", defaultValue = USER_DEFAULT_SORT_DIR, required = false) String sortDir
     ){
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -58,22 +131,101 @@ public class UserController {
         return new ResponseEntity<>(this.userService.findAll(pageNumber, pageSize, sortBy, sortDir), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a User by dni")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User Found",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDTO.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "No permission to apply",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "User Not Found",
+                    content = @Content
+            )
+    })
     @GetMapping(ENDPOINT_USER_DNI)
-    ResponseEntity<UserDTO> getByDni(@PathVariable("dni") String dni){
+    ResponseEntity<UserDTO> getByDni(
+            @Parameter(description = "User DNI to search")
+            @PathVariable("dni") String dni
+    ){
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
         this.userService.validateUserRequestByDni(dni, userEmail);
         return new ResponseEntity<>(this.mapperUser.mapUserDTO(this.userService.getByDni(dni)), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get a User by email")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User Found",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDTO.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "No permission to apply",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "User Not Found",
+                    content = @Content
+            )
+    })
     @GetMapping(ENDPOINT_USER_EMAIL)
-    ResponseEntity<UserDTO> getByEmail(@PathVariable("email") String email){
+    ResponseEntity<UserDTO> getByEmail(
+            @Parameter(description = "User email to search")
+            @PathVariable("email") String email
+    ){
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
         this.userService.validateUserRequestByEmail(email, userEmail);
         return new ResponseEntity<>(this.mapperUser.mapUserDTO(this.userService.getByEmail(email)), HttpStatus.OK);
     }
 
+    @Operation(summary = "Update a User by id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User Updated",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDTO.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "No permission to apply",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "User Not Found",
+                    content = @Content
+            )
+    })
     @PutMapping(ENDPOINT_USER_ID)
-    ResponseEntity<UserDTO> updateOne(@PathVariable("id") Long id, @Valid @RequestBody UserDTO userDTO, BindingResult bindingResult){
+    ResponseEntity<UserDTO> updateOne(
+            @Parameter(description = "User ID to be updated")
+            @PathVariable("id") Long id,
+            @Valid @RequestBody UserDTO userDTO, BindingResult bindingResult
+    ){
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
         this.userService.validateUserRequestById(id, userEmail);
         if(bindingResult.hasErrors()){
@@ -82,8 +234,34 @@ public class UserController {
         return new ResponseEntity<>(this.mapperUser.mapUserDTO(this.userService.updateOne(id, userDTO)), HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete a User by id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User Deleted",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = UserDTO.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "No permission to apply",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "User Not Found",
+                    content = @Content
+            )
+    })
     @DeleteMapping(ENDPOINT_USER_ID)
-    ResponseEntity<String> deleteOne(@PathVariable("id") Long id){
+    ResponseEntity<String> deleteOne(
+            @Parameter(description = "User ID to be deleted")
+            @PathVariable("id") Long id
+    ){
         String userEmail = SecurityContextHolder.getContext().getAuthentication().getName();
         this.userService.validateUserRequestById(id, userEmail);
         this.userService.deleteOne(id);
