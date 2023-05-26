5b279446b7e45b86f4e9a9e9dd4ff0996eb89305
Jhon Quitian <jhon.quitian@udea.edu.co>
2023-05-26 12:24:40 -0500

    Test: create test to save a user

--- src/test/java/com/orderpicker/user/application/usecase/UserServiceTest.java
+++ src/test/java/com/orderpicker/user/application/usecase/UserServiceTest.java
@@ -6,11 +6,20 @@ import com.orderpicker.user.domain.model.User;
 import com.orderpicker.user.domain.repository.UserRepository;
 import com.orderpicker.user.infrastructure.dto.UserDTO;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = "test")
@@ -53,5 +62,20 @@ public class UserServiceTest {
                 .phone("273428347782")
                 .role(Role.USER)
                 .build();
+    }
+
+    @DisplayName("Test UserService, Test to save a user")
+    @Test
+    void createOne(){
+        given(this.userRepository.findByDni(this.userDTO.getDni())).willReturn(Optional.empty());
+        given(this.userRepository.findByEmail(this.userDTO.getEmail())).willReturn(Optional.empty());
+        given(this.encryptService.encryptPassword(anyString())).willReturn("hash password");
+        given(this.mapperUser.mapUser(any(UserDTO.class))).willReturn(user);
+        given(this.userRepository.save(any(User.class))).willReturn(user);
+
+        User userSaved = this.userService.save(this.userDTO);
+
+        assertNotNull(userSaved);
+        assertNotEquals(Role.ADMIN, userSaved.getRole());
     }
 }
